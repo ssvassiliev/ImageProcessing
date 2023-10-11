38a9c1ab6cd356fb428a661447e8f2729cbf6f96
Serguiei Vassiliev <svassiliev1@gmail.com>
2023-10-11 19:53:40 -0300

    added radiometric corrections

---
+++ setvars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# module load gcc/9.3.0 opencv/4.8.0 gdal/3.5.1
+cd /home/svassili/projects/def-svassili/svassili/ODM/ImageProcessing
+source ../env-micasense/bin/activate
+export PATH=$PATH:$HOME/projects/def-svassili/svassili/ODM/Image-ExifTool-12.67
+export LD_LIBRARY_PATH=$HOME/projects/def-svassili/svassili/ODM/libzbar/lib
+

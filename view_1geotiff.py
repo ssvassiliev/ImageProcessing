a3b2d1b3189c327e57b571c0fe6a010b94ea5ca1
Serguiei Vassiliev <svassiliev1@gmail.com>
2023-10-14 09:03:08 -0300

    remove negative values

--- view_1geotiff.py
+++ view_1geotiff.py
@@ -1,12 +1,16 @@
 import sys
 import matplotlib.pyplot as plt
 from osgeo import gdal
+import numpy as np
 
 band=int(sys.argv[2])
 dataset = gdal.Open(sys.argv[1], gdal.GA_ReadOnly)
 
 b1 = dataset.GetRasterBand(band)
 arr = b1.ReadAsArray()
+arr[np.where(arr<0)]=0
+#arr[np.where(arr>0.4)]=0.0
 
 plt.imshow(arr)
+plt.colorbar()
 plt.show()

34b576c1d57be482a099ad713835731e1c30c312
Serguiei Vassiliev <svassiliev1@gmail.com>
2023-10-16 19:27:30 -0300

    renamed Panels.py

--- check_panels.py
+++ check_panels.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 
-# # Panels
-# This notebook shows usage for the Panel class.  This type is useful for detecting MicaSense calibration panels and extracting information about the lambertian panel surface.
-
-
-import os, glob
+import os, glob, argparse
 from micasense.image import Image
 from micasense.panel import Panel
 import micasense.plotutils as plotutils
 
-imagePath = "/home/svassili/projects/def-svassili/svassili/ODM/ImageProcessing/data/panels"
+parser = argparse.ArgumentParser(
+    prog='check_panels',
+    description='Checks images for presense of panels',  
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter  
+)
+parser.add_argument('path2panels', default=os.getcwd(), type=str, help="Path to project")
+args=parser.parse_args()
 
+imagePath = args.path2panels
+
+count_good=0
+count_bad=0
 for i in  sorted(glob.glob(os.path.join(imagePath,'*.*'))):
     img = Image(i)
     panel = Panel(img)
@@ -18,13 +24,15 @@ for i in  sorted(glob.glob(os.path.join(imagePath,'*.*'))):
     print(f"\nFile: {i}")
     if not panel.panel_detected():
         print("Panel Not Detected!")
+        count_bad += 1
     else:
-        print("Detected panel serial: {}".format(panel.serial)) 
+        print("Panel detected, serial: {}".format(panel.serial)) 
         mean, std, num, sat_count = panel.raw()
         print("Extracted Panel Statistics:")
         print("Mean: {}".format(mean))
         print("Standard Deviation: {}".format(std))
         print("Panel Pixel Count: {}".format(num))
         print("Saturated Pixel Count: {}".format(sat_count))
+        count_good += 1
 
-
+print(f'\n{count_good} panels OK, {count_bad} panels not detected')
